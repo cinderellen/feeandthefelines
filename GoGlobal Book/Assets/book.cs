fe3c84f42f9d7635ab59aaf069e93a1ab546ed1f
Ellen Long <ellen.renee.01@gmail.com>
2017-04-16 15:37:24 +1000

    added prelim unity code

---
+++ GoGlobal Book/Assets/book.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class book : MonoBehaviour {
+
+	public GameObject phrase1;
+	public GameObject phrase2;
+	public GameObject phrase3;
+
+	public int phraseCount = 1;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")){
+			phraseCount++;
+		}
+
+
+		if (phraseCount == 1) {
+			phrase1.SetActive (true);
+		} else if (phraseCount == 2) {
+			phrase1.SetActive (false);
+			phrase2.SetActive (true);
+		} else if (phraseCount == 3) {
+			phrase2.SetActive (false);
+			phrase3.SetActive (true);
+		} 
+
+		else if (phraseCount == 4) {
+			phraseCount = 1;
+		}
+
+
+	}
+}
+
+
